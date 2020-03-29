af3d8f646b8564bb517e6f08c224472db79551b2
星垂月朦朧 <54883978+EddieMap1e@users.noreply.github.com>
2020-03-29 12:16:18 +0800

    修改为mobile端

--- 2048/assets/scripts/gameOver.js
+++ 2048/assets/scripts/gameOver.js
@@ -11,17 +11,17 @@ cc.Class({
 
     init() {
         let failScore = cc.sys.localStorage.getItem('failScore');
-        if (failScore === 'null') failScore = 0;
+        if (failScore === null) failScore = 0;
         cc.log(failScore);
         this.failScore.string = "分数 : " + failScore;
         let bestScore = cc.sys.localStorage.getItem('bestScore');
-        if (bestScore === 'null' || parseInt(failScore) > parseInt(bestScore)) bestScore = failScore;
+        if (bestScore == 0 || bestScore === null || parseInt(failScore) > parseInt(bestScore)) bestScore = failScore;
         cc.sys.localStorage.setItem('bestScore', bestScore);
         this.bestScore.string = "最高分 : " + bestScore;
         cc.log(bestScore);
     },
 
-    retry(event,customEventData){
+    retry(event, customEventData) {
         cc.director.loadScene('gameStart');
     }
 });
