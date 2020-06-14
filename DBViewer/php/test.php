1884b12a07886bb96dc11ee636febb445a1725b3
星垂月朦朧 <54883978+EddieMap1e@users.noreply.github.com>
2020-06-14 19:06:06 +0800

    调试

--- DBViewer/php/test.php
+++ DBViewer/php/test.php
@@ -1,4 +1,10 @@
 <?php
     $table_name=isset($_POST['table_name'])?htmlspecialchars($_POST['table_name']):'';
-    echo json_encode($table_name);
+    require("./sqlFunctions.php");
+	$conn=mConnectSql();
+	if(!$conn){
+		echo json_encode("连接数据库失败");
+		exit;
+    }
+    echo json_encode('fuck');
 ?>
